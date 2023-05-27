85144ae2697c3c01a1613a667eca9bf1a30e4cef
KatsiarynaBaiko <katerinabayko@gmail.com>
2023-05-27 14:03:04 +0300

    Lessons 37: created button components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+//step 3 Button
+import Button, { ButtonTypes } from './components/Button';
+
+// function App() {
+//   return (
+//     <div></div>
+//   );
+// }
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* //step 4 Button */}
+      {/* <Button type={ButtonTypes.Primary} title={'Primary'} onClick={()=> {}} disabled={true}/> или */}
+      <Button disabled type={ButtonTypes.Primary} title={'Primary'} onClick={()=> {}}/> 
+      <Button type={ButtonTypes.Secondary} title={'Secondary'} onClick={()=> {}}/>
+      <Button type={ButtonTypes.Error} title={'Error'} onClick={()=> {}}/>
     </div>
   );
 }
---
+++ src/components/Button/index.ts
@@ -0,0 +1,8 @@
+import Button from "./Button";
+// так делаем всегда при создании компонента 
+
+// step 3
+export { default } from  './Button';
+
+// step 4
+export {ButtonTypes} from './Button';
